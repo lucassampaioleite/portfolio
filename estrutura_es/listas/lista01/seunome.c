b5665918f767ef2b629120753108c40f801965b0
lucassampaioleite <lucas.sampaio.leite@gmail.com>
2026-05-09 09:54:08 -0300

    added lista01 in algoritmos_es

---
+++ estrutura_es/listas/lista01/seunome.c
@@ -0,0 +1,84 @@
+//1. Troca de valores sem variável auxiliar
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//2.Remoção de caracteres repetidos consecutivos
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//3.Cadastro de alunos com média
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//4.Cadastro dinâmico de alunos
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//1003 - Soma Simples
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1009 - Salário com Bônus
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1017 - Gasto de Combustível
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1018 - Cédulas
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1174 - Seleçao em Vetor I
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1253 - Cifra de César do Beecrowd
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
