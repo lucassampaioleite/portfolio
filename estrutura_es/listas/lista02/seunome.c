cec0e8ba11a9b0b3ad9f7edc492165fff3556156
lucassampaioleite <lucas.sampaio.leite@gmail.com>
2026-05-21 22:30:38 -0300

    added lista_02 in estrutura_es

---
+++ estrutura_es/listas/lista02/seunome.c
@@ -0,0 +1,66 @@
+//1. Implemente uma Lista a partir de um vetor estático, considerando todos os métodos da interface da lista descrita.
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//2. Implemente uma Lista a partir de um vetor dinâmico, considerando todos os métodos da interface da lista descrita.
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//3. Implemente uma Lista encadeada simples considerando todos os métodos da interface da lista descrita.
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+
+//4. Implemente uma Lista encadeada dupla considerando todos os métodos da interface da lista descrita.
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//5. Tomando como base uma das listas implementadas, implemente uma fila com as funcionalidades: void enqueue(Aluno aluno), Aluno dequeue(), Aluno front(), int isEmpty(), int size() e int clear(). 
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//6. Tomando como base uma das listas implementadas, implemente uma pilha com os métodos/funções: void push(Aluno aluno), Aluno pop(), Aluno peak(), int inEmpty() e int size().
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1340 - Eu Posso Adivinhar a Estrutura de Dados!
+
+/*
+
+Cole aqui a sua resposta
+
+*/
+
+//1523 - Estacionamento Linear
+
+/*
+
+Cole aqui a sua resposta
+
+*/
